1f0a2a3751e8320751f6fed6ab870228900b9972
Paulo Oliveira <paulooliveira@pecege.com>
2018-03-12 11:04:47 -0300

    machine

--- controllers/machine.js
+++ controllers/machine.js
@@ -10,17 +10,17 @@ module.exports = function(app) {
 
         var errors = req.validationErrors();
         if(errors)
-            return res.status(400).send(errors);        
+            return res.status(400).send(errors);
 
         var connection = app.database.connection();
         var machine = new app.database.repository.machine(connection);
 
         machine.autenticateToken(bodyData.token, function(exception, result) {
             if(exception) {
-                return res.status(400).send(exception);
+                return res.status(500).send(exception);
             }
             if(!result[0]) {
-                return res.send('0'); //seguindo modelo do thingspeak
+                return res.status(401).send('Token inválido');
             }
 
             //null na data
@@ -30,9 +30,9 @@ module.exports = function(app) {
             delete bodyData.token;
             machine.save(bodyData, function(exception, result) {
                 if(exception) {
-                    res.status(400).send(exception);
+                    return res.status(500).send(exception);
                 }                 
-                res.send(bodyData);
+                return res.send(bodyData);
             });          
         });        
     });
@@ -46,17 +46,17 @@ module.exports = function(app) {
 
         var errors = req.validationErrors();
         if(errors)
-            return res.status(400).send(errors);        
+            return res.status(400).send(errors);       
 
         var connection = app.database.connection();
         var machine = new app.database.repository.machine(connection);
 
         machine.autenticateToken(bodyData.token, function(exception, result) {
             if(exception) {
-                return res.status(400).send(exception);
+                return res.status(500).send(exception);
             }
             if(!result[0]) {
-                return res.send('0'); //seguindo modelo do thingspeak
+                return res.status(401).send('Token inválido');
             }
 
             //null na data
@@ -66,12 +66,43 @@ module.exports = function(app) {
             delete bodyData.token;
             machine.update(bodyData, function(exception, results, fields) {
                 if(exception) {
-                    res.status(400).send(exception);
+                    return res.status(500).send(exception);
                 }    
-                res.send(bodyData);           
+                return res.send(bodyData);           
             });            
         });        
     });
+
+    app.post('/oee/api/machine/delete', function(req, res) {
+        var bodyData = req.body;
+
+        //cria asserts para validação
+        req.assert('code', 'Preencha o código corretamente.').notEmpty();
+
+        var errors = req.validationErrors();
+        if(errors)
+            return res.status(400).send(errors);         
+
+        var connection = app.database.connection();
+        var machine = new app.database.repository.machine(connection);
+
+        machine.autenticateToken(bodyData.token, function(exception, result) {
+            if(exception) {
+                res.status(500).send(exception);
+            }
+            if(!result[0]) {
+                return res.status(401).send('Token inválido');
+            }
+                        
+            delete bodyData.token;
+            machine.delete(bodyData, function(exception, results, fields) {
+                if(exception) {
+                    return res.status(500).send(exception);
+                }
+                return res.send(bodyData);
+            });            
+        });        
+    });    
 
     app.get('/oee/api/machine/list', function(req, res) {        
         var connection = app.database.connection();
@@ -79,7 +110,7 @@ module.exports = function(app) {
         
         machine.list(function(exception, result) {
             if(exception) {
-                return res.status(400).send(exception);
+                return res.status(500).send(exception);
             }
             res.send(result);
             connection.end();
