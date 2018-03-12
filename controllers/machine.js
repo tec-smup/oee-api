edde2352016b9528f90b5fb7d32f6d1690991fab
Paulo Oliveira <paulo.oliveira278@gmail.com>
2018-03-11 23:00:32 -0300

    machine

--- controllers/machine.js
+++ controllers/machine.js
@@ -28,17 +28,46 @@ module.exports = function(app) {
             bodyData.last_maintenance = bodyData.last_maintenance || null;                       
             
             delete bodyData.token;
+            machine.save(bodyData, function(exception, result) {
+                if(exception) {
+                    res.status(400).send(exception);
+                }                 
+                res.send(bodyData);
+            });          
+        });        
+    });
+
+    app.post('/oee/api/machine/update', function(req, res) {
+        var bodyData = req.body;
+
+        //cria asserts para validação
+        req.assert('code', 'Preencha o código corretamente.').notEmpty();
+        req.assert('name', 'Preencha o nome corretamente.').notEmpty();
+
+        var errors = req.validationErrors();
+        if(errors)
+            return res.status(400).send(errors);        
+
+        var connection = app.database.connection();
+        var machine = new app.database.repository.machine(connection);
+
+        machine.autenticateToken(bodyData.token, function(exception, result) {
+            if(exception) {
+                return res.status(400).send(exception);
+            }
+            if(!result[0]) {
+                return res.send('0'); //seguindo modelo do thingspeak
+            }
+
+            //null na data
+            bodyData.next_maintenance = bodyData.next_maintenance || null;                       
+            bodyData.last_maintenance = bodyData.last_maintenance || null;                       
+            
+            delete bodyData.token;
             machine.update(bodyData, function(exception, results, fields) {
                 if(exception) {
-                    return res.status(400).send(exception);
-                }
-                if(results.changedRows == 0) {
-                    machine.save(bodyData, function(exception, result) {
-                        if(exception) {
-                            return res.status(400).send(exception);
-                        }                 
-                    });
-                }      
+                    res.status(400).send(exception);
+                }    
                 res.send(bodyData);           
             });            
         });        
