b2fc0209b23099cab01448a49565cdfaca9b38ca
Paulo Oliveira <paulooliveira@pecege.com>
2018-03-16 17:00:55 -0300

    gcloud

--- database/connection.js
+++ database/connection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 function connection() {
     return mysql.createConnection({
         host: 'localhost',
-        user: 'uoee',
-        password: '!2o0e1e7#',
-        database: 'oee',
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASSWORD,
+        database: process.env.SQL_DATABASE,
         timezone: 'utc'
     });
 }
