1d99a954a8ea8ed26a86b5919f6909f3fa301c64
Paulo Oliveira <paulooliveira@pecege.com>
2018-05-02 14:56:12 -0300

    app.yaml cors

--- config/custom-express.js
+++ config/custom-express.js
@@ -8,6 +8,7 @@ const jwtSecret = process.env.JWT_SECRET || '';
 const env = process.env.NODE_ENV || 'dev';
 const pubsub_verification_token = process.env.PUBSUB_VERIFICATION_TOKEN || '';
 const pubsub_topic = process.env.PUBSUB_TOPIC || '';
+const corsUrl = process.env.CORS_URL || 'http://localhost:4200';
 
 module.exports = function() {
     var app = express(); 
@@ -31,14 +32,12 @@ module.exports = function() {
     });    
     app.use('/api', router);
 
-    //somente para teste
-    if(env == 'dev') {
-        app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
-    } 
+    //habilita cors
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", corsUrl);
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
     consign()
         .include('controllers')
